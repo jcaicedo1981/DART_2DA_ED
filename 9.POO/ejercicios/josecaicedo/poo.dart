c6d7dd25a7775b742dc0a904aff38dfc80589ec7
jcaicedo1981 <jcaicedofix@gmail.com>
2024-05-08 20:02:34 -0500

    cla8

---
+++ 9.POO/ejercicios/josecaicedo/poo.dart
@@ -0,0 +1,23 @@
+import '../../code/3_herencia_extends.dart';
+
+void main(){
+
+print(Perro("rojo", "juamn", "10"));
+
+final perroObjeto = Animal();
+final gatoObjeto = Animal();
+final aveObjeto = Animal();
+
+}
+
+/// clases
+/// class <NOmbre de la clase>{}
+/// 
+/// Perro->nombre,edad, color
+/// Gatto->nombre,edad, color
+/// Ave->nombre,edad, color
+class Animal{
+
+
+
+}
