0bcf317ecd859514749d729e50ecaeeadcdb89ed
jcaicedo1981 <jcaicedofix@gmail.com>
2024-05-13 21:05:15 -0500

    cl11

---
+++ 10.async/ejercicios/richar_cangui/jose caicedo/async_fuction.dart
@@ -0,0 +1,61 @@
+void main()
+{
+
+//saludar();
+print(await saludar());
+}
+
+////programacion asincrona
+///async  - awit
+///
+//void saludar(){
+//print ("hola mundo");
+
+//}
+
+//Future<void> saludar() async{
+//print("hola mundo 2");
+
+//}
+
+Future<String> saludar() async{
+//print("hola mundo 2");
+
+return "hola mundo 2";
+
+}
+
+Future<void> finDEAnio() async{
+
+for(int i=10;i>0;i--){
+
+  print(i);
+  await Future.delayed(Duration(seconds: 1));
+}
+
+print("feliz anio");
+
+ }
+
+ Future<String> nombreUsuario() Async{
+  try {
+    print("se ejecuta");
+    
+      } catch (err) {
+    print("Ocurrio un error");
+      //return '';
+      rethrow;
+  }
+  finally{
+    print("siempre se jecuta");
+  }
+
+ }
+
+ Future<String> getNombre() async{
+
+  await Future.delayed(Duration(seconds: 2));
+  throw Exception("Error .....");
+  //return "Usuario patito";
+ }
+
---
+++ 7.functions/ejercicios/jose caicedo/stream.dart
@@ -0,0 +1,20 @@
+void (){
+
+  ///Stream
+  ///
+  final Streamuno = Stream.fromIterable([19,9,8,7,6,5,4,3,2,1,0]);
+
+}
+
+Future<int> sumaStream(Stream<int> stream) async{
+var sum=0;
+await for(int value in stream){
+
+  print("valor: $value");
+  await Future.delayed(Duration(milliseconds: 100));
+  sum += value;
+}
+
+return sum;
+
+}
